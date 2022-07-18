1760c1b38ca729f311e9754c98406372f50192db
jan_bogdziewicz_comarch <jan.bogdziewicz@comarch.pl>
2022-07-18 10:53:39 +0200

    initial commit

---
+++ src/main/java/org/janbogdziewicz/tabu/TabuApplication.java
@@ -0,0 +1,13 @@
+package org.janbogdziewicz.tabu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabuApplication.class, args);
+	}
+
+}
