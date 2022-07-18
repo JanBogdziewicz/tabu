3c8259ed63368406989359a9f50a7d4abd6d7186
jan_bogdziewicz <jan.bogdziewicz@comarch.pl>
2022-07-18 14:49:10 +0200

    basic 3

--- src/main/java/org/janbogdziewicz/tabu/TabuApplication.java
+++ src/main/java/org/janbogdziewicz/tabu/TabuApplication.java
@@ -6,14 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootApplication
-//@EntityScan("org.janbogdziewicz.model")
 public class TabuApplication {
 	private final CardService cardService;
 
